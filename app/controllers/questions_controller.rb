cca3f8da99a96b537a03ba8c90b0b229d8039d36
Aya Kashihara <aya.kashihara@gmail.com>
2019-03-25 16:14:56 +0900

    question answer implemented.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,20 @@ class QuestionsController < ApplicationController
   def ask
   end
 
+  ANSWERS = {
+    1 => 'Great!',
+    2 => 'Silly question, get dressed and go to work!',
+    3 => 'I don\'t care, get dressed and go to work!'
+  }
+
   def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      return @answer = ANSWERS[1]
+    elsif @question.include? '?'
+      return @answer = ANSWERS[2]
+    else
+      return @answer = ANSWERS[3]
+    end
   end
 end
